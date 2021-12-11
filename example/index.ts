9a91e67be10a2c36c6a044bd0f7bd84d491ac2d8
AdamPiotrowsky <AdamPiotrowskiC@gmail.com>
2021-12-11 23:33:37 +0100

    npm best practise is different, do it tomorrow

--- example/index.ts
+++ example/index.ts
@@ -7,26 +7,23 @@ const rawData: Employee[] = JSON.parse(
   fs.readFileSync(path.join(__dirname, "../MOCK_DATA.json")).toString()
 );
 const data = rawData.map((item) => new Employee(item));
-console.log(data[0].valueOf());
 const myDB = new TreeDB<Employee>();
 
 const unsubscribe = myDB
   .subscribe()
   .AddAfterAddToDb(({ newValue }) => console.log(newValue));
-unsubscribe();
-// const unsubscribe2 = myDB
-//   .subscribe()
-//   .AddBeforeAddToDb(({ newValue, value }) =>
-//     console.log(`newValue:${newValue} \n value:${value}`)
-//   );
-// const sub = myDB.subscribe();
+const unsubscribe2 = myDB
+  .subscribe()
+  .AddBeforeAddToDb(({ newValue, value }) =>
+    console.log(`newValue:${newValue} \n value:${value}`)
+  );
 
 for (let i = 0; i < 50; i++) {
   myDB.push(data[i]);
 }
 // last id:50
-// unsubscribe();
-// unsubscribe();
+unsubscribe();
+unsubscribe2();
 
 for (let i = 51; i < 100; i++) {
   myDB.push(data[i]);
@@ -34,5 +31,5 @@ for (let i = 51; i < 100; i++) {
 // still last id: 50
 
 console.log(myDB.getMax());
-// console.log(myDB.getInOrder());
-// myDB.clg();
+console.log("GET IN ORDER");
+console.log(myDB.getInOrder());
