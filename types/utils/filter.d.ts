65b054c79f46e7e110be063466e0c0aa84948426
danielPaszek <78235127+danielPaszek@users.noreply.github.com>
2022-01-24 19:24:32 +0100

    Delete types directory

--- types/utils/filter.d.ts
+++
@@ -1,6 +0,0 @@
-import { IFilter } from "../types";
-export declare class Filter<DataType> implements IFilter<DataType> {
-    private filters;
-    addFilter(filter: (item: DataType) => boolean): () => void;
-    isAllowed(item: DataType): boolean;
-}
